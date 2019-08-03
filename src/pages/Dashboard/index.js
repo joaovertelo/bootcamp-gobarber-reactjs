e06140a2e10b851dd02120e9b883746641835e55
PabloMelo11 <pablomelo418@gmail.com>
2019-08-02 23:10:45 -0300

    Adicionar notifications

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -4,6 +4,6 @@ import api from '~/services/api';
 // import { Container } from './styles';
 
 export default function Dashboard() {
-  api.get('/appointments');
+  api.get('appointments');
   return <h1>Dashboard</h1>;
 }
