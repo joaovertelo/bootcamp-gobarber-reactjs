9934b9348d85d2977b3aa489db31459fa087e8c2
PabloMelo11 <pablomelo418@gmail.com>
2019-08-03 22:35:29 -0300

    Front-end finalizado

--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -1,18 +1,9 @@
 import styled from 'styled-components';
 import { darken } from 'polished';
 
-export const Wrapper = styled.div`
-  height: 100%;
-  background: linear-gradient(-90deg, #0083b0, #00b4db);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-export const Content = styled.div`
-  width: 100%;
-  max-width: 315px;
-  text-align: center;
+export const Container = styled.div`
+  max-width: 600px;
+  margin: 50px auto;
 
   form {
     display: flex;
@@ -40,6 +31,13 @@ export const Content = styled.div`
       font-weight: bold;
     }
 
+    hr {
+      border: 0;
+      height: 1px;
+      background: rgba(255, 255, 255, 0.2);
+      margin: 10px 0 20px;
+    }
+
     button {
       margin: 5px 0 0;
       height: 44px;
@@ -55,16 +53,22 @@ export const Content = styled.div`
         background: ${darken(0.03, '#1abc9c')};
       }
     }
+  }
 
-    a {
-      color: #fff;
-      margin-top: 15px;
-      font-size: 16px;
-      opacity: 0.8;
+  > button {
+    width: 100%;
+    margin: 10px 0 0;
+    height: 44px;
+    background: #f64c75;
+    font-weight: bold;
+    border: 0;
+    color: #fff;
+    border-radius: 4px;
+    font-size: 16px;
+    transition: background 0.2s;
 
-      &:hover {
-        opacity: 1;
-      }
+    &:hover {
+      background: ${darken(0.08, '#f64c75')};
     }
   }
 `;
